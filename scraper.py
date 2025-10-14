d6fdd4c55503fabe86b892095372e8c0383690eb
Gabriel Sá <gabriel10043@live.com>
2025-10-14 11:17:03 -0300

    add scraper script

---
+++ scraper.py
@@ -0,0 +1,30 @@
+import os
+import sys
+import tweepy
+from dotenv import load_dotenv
+
+def panic(msg):
+    print(msg)
+    exit(1) 
+
+def load():
+    if len(sys.argv) < 2:
+        panic("Usage: python ./scraper.py <query>")
+    query = sys.argv[1]
+
+    envs = load_dotenv() 
+    if not envs:
+        panic("scrape: Missing .env file")
+
+    bearer_token = os.getenv("BEARER_TOKEN")
+    if not bearer_token:
+        panic("scrape: Missing TWEEPY_BEARER in .env")
+    return query, bearer_token
+
+
+query, bearer_token = load()
+client: tweepy.Client = tweepy.Client(bearer_token=bearer_token)
+response: tweepy.Response = client.search_recent_tweets(
+        query,
+        max_results=100
+ )
