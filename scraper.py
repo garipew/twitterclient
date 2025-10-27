1fd61f811a533b4112ca5302a61527a386ee9c3a
Gabriel Sá <gabriel10043@live.com>
2025-10-27 17:52:03 -0300

    add timer for multiple requests

--- scraper.py
+++ scraper.py
@@ -1,7 +1,9 @@
 import os
 import sys
+import time
 import tweepy
 import pandas as pd
+from typing import cast
 from dotenv import load_dotenv
 
 def panic(msg):
@@ -31,25 +33,42 @@ def save_to_csv(tweets, query, filename="tweets.csv"):
     else:
         df.to_csv(filename, index=False)
 
+def collect_tweets(query, client, since_id=None, pages=5):
+    tweets = []
+
+    paginator = tweepy.Paginator(
+            client.search_recent_tweets,
+            query,
+            tweet_fields=["created_at", "author_id", "text", "public_metrics"],
+            expansions=["author_id"],
+            user_fields=["username"],
+            max_results=100,
+            since_id=since_id,
+            limit=pages
+            )
+
+    for response in paginator:
+        response = cast(tweepy.Response, response)
+        if not response.data:
+            continue
+        users = {user.id : user.username for user in response.includes["users"]}
+        for tweet in response.data:
+            tweets.append({
+                "id": tweet.id,
+                "created_at": tweet.created_at,
+                "author": users[tweet.author_id],
+                "text": tweet.text.replace("\n", " "),
+                "likes": tweet.public_metrics["like_count"],
+                })
+    return tweets
+
 query, bearer_token = load()
 client: tweepy.Client = tweepy.Client(bearer_token=bearer_token)
-response: tweepy.Response = client.search_recent_tweets(
-        query,
-        tweet_fields=["created_at", "author_id", "text", "like_count"],
-        expansions=["author_id"],
-        user_fields=["username"],
-        max_results=100
-)
-users = {user["author_id"] : user["username"] for user in response.includes["users"]}
-
-tweets = []
-for tweet in response.data:
-    tweets.append({
-        "id": tweet.id,
-        "created_at": tweet.created_at,
-        "author": users[tweet.author_id],
-        "text": tweet.text.replace("\n", " "),
-        "likes": tweet.like_count,
-        })
-
-save_to_csv(tweets, query)
+while True:
+    print("Collecting tweets...")
+    tweets = collect_tweets(query, client)
+    if(tweets):
+        save_to_csv(tweets, query)
+        print(f'Saved {len(tweets)} tweets.')
+    print('Preparing for the next request...')
+    time.sleep(900)
